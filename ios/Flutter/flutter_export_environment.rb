5104c5ea353b6067cbac1009eff20aef1ef449d4
pravinchoudhar-i <114323496+pravinchoudhar-i@users.noreply.github.com>
2025-03-11 12:10:07 +0530

    Create flutter_export_environment.rb

---
+++ ios/Flutter/flutter_export_environment.rb
@@ -0,0 +1,14 @@
+# flutter_export_environment.rb
+
+FLUTTER_ROOT="/opt/homebrew/Caskroom/flutter/3.10.6/flutter"
+FLUTTER_APPLICATION_PATH="/Users/runner/work/flutter_projects_ios/flutter_projects_ios"
+COCOAPODS_PARALLEL_CODE_SIGN="true"
+FLUTTER_TARGET="lib/main.dart"
+FLUTTER_BUILD_DIR="build"
+SYMROOT="${SOURCE_ROOT}/../build/ios"
+FLUTTER_BUILD_NAME="1.0.0"
+FLUTTER_BUILD_NUMBER="1"
+DART_OBFUSCATION="false"
+TRACK_WIDGET_CREATION="true"
+TREE_SHAKE_ICONS="false"
+PACKAGE_CONFIG=".dart_tool/package_config.json"
